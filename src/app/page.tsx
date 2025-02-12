bb1bc6a84c33f1ae6dd304553b26d62c0730950d
RuanPablo-7 <ToyaMON777@outlook.com>
2025-02-11 23:08:10 -0300

    event click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,22 @@
 
-const products = [
-  { title:'apple', isFruit: false, id:1},
-  { title:'orange', isFruit: false, id:2},
-  { title:'banana', isFruit: true, id:3},
-]
-
-
-
-
-export default function Home() {
-  const listItems = products.map(product => 
-    <li 
-    key={product.id}
-    style={{
-      color: product.isFruit ? 'green' : 'red'
-    }}
-    >
-        {product.title}
-    </li>
+"use client"
+function MyButton() {
+  function handleClick() {
+    alert('you clicked me!')
+  
+    
+  }
+  return (
+  <button onClick={handleClick}>Click me!</button>
   
   )
+}
+
+export default function Home() {
+
   return (
-      <div className="bg-white w-20 h-50 p-10 avatar" >
-        {listItems}
-      </div>
+    <div>
+      <MyButton/>
+    </div>
   );
 }
