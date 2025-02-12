10992874fc61e93d4323a85bba6d66530d13e447
RuanPablo-7 <ToyaMON777@outlook.com>
2025-02-11 22:43:57 -0300

    array products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,28 @@
-import Image from "next/image";
+
+const products = [
+  { title:'apple', isFruit: false, id:1},
+  { title:'orange', isFruit: false, id:2},
+  { title:'banana', isFruit: true, id:3},
+]
 
 
-const user = {
-      name: 'Ruan Pablo',
-      imageUrl: '/eu.jpg',
-      imageSize: 90,
-};
 
-let content: string= 'hello';
-let valor = false
-if (valor) {
-  content = <Casa/>
-} else {
-  content = <Mansao/>
-}
-
-function Casa() {
-  return(
-    <h1>eu sou uma o ruan</h1>
-  )
-}
-function Mansao() {
-  return(
-    <h1>eu sou um super heroi</h1>
-  )
-}
-
-function MyButton() {
-  return(
-    <button className="avatar">I'm button</button>
-  )
-}
 
 export default function Home() {
+  const listItems = products.map(product => 
+    <li 
+    key={product.id}
+    style={{
+      color: product.isFruit ? 'green' : 'red'
+    }}
+    >
+        {product.title}
+    </li>
+  
+  )
   return (
-      <div className="bg-white p-6">
-        <h2 className="text-black">{user.name}</h2>
-        <img
-        className="avatar " 
-        src={user.imageUrl} 
-        alt={'Photo of '+ user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-        />
-      
-      <div className="text-black">
-        {content}
-
+      <div className="bg-white w-20 h-50 p-10 avatar" >
+        {listItems}
       </div>
-     
-      </div>
-
   );
 }
